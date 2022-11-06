3fc2026f1201c6c95c9cde9837b5f13eec3a4372
caffi <caffi.senna@gmail.com>
2022-11-06 12:05:54 +0900

    feature #16
    close #16

--- resources/views/admin_entry_infos/table.blade.php
+++ resources/views/admin_entry_infos/table.blade.php
@@ -1,3 +1,70 @@
+<link rel="stylesheet" type="text/css" href="{{ url('/datatables/jquery.dataTables.css') }}">
+<script type="text/javascript" charset="utf8" src="{{ url('/datatables/jquery.dataTables.js') }}"></script>
+<script src="{{ url('/datatables/dataTables.fixedHeader.min.js') }}"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        // Setup - add a text input to each footer cell
+        $('#entryInfos-table thead tr')
+            .clone(true)
+            .addClass('filters')
+            .appendTo('#entryInfos-table thead');
+
+        var table = $('#entryInfos-table').DataTable({
+            orderCellsTop: true,
+            fixedHeader: true,
+            initComplete: function() {
+                var api = this.api();
+
+                // For each column
+                api
+                    .columns()
+                    .eq(0)
+                    .each(function(colIdx) {
+                        // Set the header cell to contain the input element
+                        var cell = $('.filters th').eq(
+                            $(api.column(colIdx).header()).index()
+                        );
+                        var title = $(cell).text();
+                        $(cell).html('<input type="text" placeholder="' + title +
+                                            '" style="width:60px" />');
+
+                        // On every keypress in this input
+                        $(
+                                'input',
+                                $('.filters th').eq($(api.column(colIdx).header()).index())
+                            )
+                            .off('keyup change')
+                            .on('keyup change', function(e) {
+                                e.stopPropagation();
+
+                                // Get the search value
+                                $(this).attr('title', $(this).val());
+                                var regexr =
+                                    '({search})'; //$(this).parents('th').find('select').val();
+
+                                var cursorPosition = this.selectionStart;
+                                // Search the column for that value
+                                api
+                                    .column(colIdx)
+                                    .search(
+                                        this.value != '' ?
+                                        regexr.replace('{search}', '(((' + this.value +
+                                            ')))') :
+                                        '',
+                                        this.value != '',
+                                        this.value == ''
+                                    )
+                                    .draw();
+
+                                $(this)
+                                    .focus()[0]
+                                    .setSelectionRange(cursorPosition, cursorPosition);
+                            });
+                    });
+            },
+        });
+    });
+</script>
 <div class="table-responsive">
     <table class="uk-table uk-table-divider" id="entryInfos-table">
         <thead>
@@ -5,9 +72,9 @@
                 <th>氏名</th>
                 <th>SC期数</th>
                 <th>課程別期数</th>
-                <th>団委員長確認</th>
-                <th>地区コミ確認</th>
-                <th>委員会確認</th>
+                <th>団委員長</th>
+                <th>地区コミ</th>
+                <th>委員会</th>
                 <th>操作</th>
             </tr>
         </thead>
@@ -37,11 +104,12 @@
                             @if (isset($entryInfo->entry_info->ais_checked_at))
                                 {{ $entryInfo->entry_info->ais_checked_at->format('Y-m-d') }}
                             @else
-                                <a href="{{ url('/admin/ais_check/?id=') }}{{ $entryInfo->entry_info->id }}" class=" uk-button uk-button-primary">承認する</a>
+                                <a href="{{ url('/admin/ais_check/?id=') }}{{ $entryInfo->entry_info->id }}" class=" uk-button uk-button-primary"
+                                    onclick="return confirm('{{ $entryInfo->name }}さんを承認しますか?')">承認</a>
                             @endif
                         </td>
-                        <td width="120">
-                            {!! Form::open(['route' => ['admin_entryInfos.destroy', $entryInfo->id], 'method' => 'delete']) !!}
+                        <td>
+                            {!! Form::open(['route' => ['admin_entryInfos.destroy', $entryInfo->entry_info->id], 'method' => 'delete']) !!}
                             <div class='btn-group'>
                                 <a href="{{ url('/admin/pdf/?id=') }}{{ $entryInfo->entry_info->user_id }}"
                                     class='btn btn-default'>
@@ -58,7 +126,7 @@
                                 {!! Form::button('<i class="far fa-trash-alt"></i>', [
                                     'type' => 'submit',
                                     'class' => 'btn btn-danger btn-xs',
-                                    'onclick' => "return confirm('本当に削除しますか?')",
+                                    'onclick' => "return confirm('{$entryInfo->name}さんの情報を削除しますか?')",
                                 ]) !!}
                             </div>
                             {!! Form::close() !!}
@@ -69,3 +137,8 @@
         </tbody>
     </table>
 </div>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#entryInfos-table').DataTable();
+    });
+</script>
