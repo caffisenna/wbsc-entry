e1eadfd0fbfef0a234eae60505d9563f56545682
caffi <caffi.senna@gmail.com>
2022-11-06 12:08:00 +0900

    feature #17
    close #17

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -10,3 +10,206 @@
  */
 
 
+namespace App\Models{
+/**
+ * Class Entry_info
+ *
+ * @package App\Models
+ * @version October 15, 2022, 10:45 pm JST
+ * @property string $furigana
+ * @property string $gender
+ * @property string $bs_id
+ * @property string $prefecture
+ * @property string $district
+ * @property string $dan
+ * @property string $troop
+ * @property string $troop_role
+ * @property string $cell_phone
+ * @property string $zip
+ * @property string $address
+ * @property string $district_role
+ * @property string $prefecture_role
+ * @property string $scout_camp
+ * @property string $bs_basic_course
+ * @property string $wb_basic1_category
+ * @property string $wb_basic1_number
+ * @property string $wb_basic1_date
+ * @property string $wb_adv1_category
+ * @property string $wb_adv1_number
+ * @property string $wb_adv1_date
+ * @property string $service_hist1_role
+ * @property string $service_hist1_term
+ * @property string $health_illness
+ * @property string $health_memo
+ * @property string $commi_checked_at
+ * @property string $ais_checked_at
+ * @property string $gm_checked_at
+ * @property int $id
+ * @property int|null $user_id
+ * @property string $sc_number
+ * @property string $division_number
+ * @property \Illuminate\Support\Carbon $birthday
+ * @property string|null $wb_basic2_category
+ * @property string|null $wb_basic2_number
+ * @property string|null $wb_basic2_date
+ * @property string|null $wb_basic3_category
+ * @property string|null $wb_basic3_number
+ * @property string|null $wb_basic3_date
+ * @property string|null $wb_basic4_category
+ * @property string|null $wb_basic4_number
+ * @property string|null $wb_basic4_date
+ * @property string|null $wb_basic5_category
+ * @property string|null $wb_basic5_number
+ * @property string|null $wb_basic5_date
+ * @property string|null $wb_adv2_category
+ * @property string|null $wb_adv2_number
+ * @property string|null $wb_adv2_date
+ * @property string|null $wb_adv3_category
+ * @property string|null $wb_adv3_number
+ * @property string|null $wb_adv3_date
+ * @property string|null $wb_adv4_category
+ * @property string|null $wb_adv4_number
+ * @property string|null $wb_adv4_date
+ * @property string|null $wb_adv5_category
+ * @property string|null $wb_adv5_number
+ * @property string|null $wb_adv5_date
+ * @property string|null $service_hist2_role
+ * @property string|null $service_hist2_term
+ * @property string|null $service_hist3_role
+ * @property string|null $service_hist3_term
+ * @property string|null $service_hist4_role
+ * @property string|null $service_hist4_term
+ * @property string|null $service_hist5_role
+ * @property string|null $service_hist5_term
+ * @property string|null $assignment_sc
+ * @property string|null $assignment_division
+ * @property string|null $face_picture
+ * @property string|null $uuid
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property-read \App\Models\User|null $user
+ * @method static \Database\Factories\Entry_infoFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Entry_info onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereAisCheckedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereAssignmentDivision($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereAssignmentSc($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereBirthday($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereBsBasicCourse($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereBsId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereCellPhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereCommiCheckedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereDan($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereDistrict($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereDistrictRole($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereDivisionNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereFacePicture($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereFurigana($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereGender($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereGmCheckedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereHealthIllness($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereHealthMemo($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info wherePrefecture($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info wherePrefectureRole($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereScNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereScoutCamp($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereServiceHist1Role($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereServiceHist1Term($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereServiceHist2Role($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereServiceHist2Term($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereServiceHist3Role($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereServiceHist3Term($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereServiceHist4Role($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereServiceHist4Term($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereServiceHist5Role($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereServiceHist5Term($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereTroop($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereTroopRole($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereUuid($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbAdv1Category($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbAdv1Date($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbAdv1Number($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbAdv2Category($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbAdv2Date($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbAdv2Number($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbAdv3Category($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbAdv3Date($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbAdv3Number($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbAdv4Category($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbAdv4Date($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbAdv4Number($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbAdv5Category($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbAdv5Date($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbAdv5Number($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbBasic1Category($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbBasic1Date($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbBasic1Number($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbBasic2Category($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbBasic2Date($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbBasic2Number($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbBasic3Category($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbBasic3Date($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbBasic3Number($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbBasic4Category($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbBasic4Date($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbBasic4Number($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbBasic5Category($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbBasic5Date($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereWbBasic5Number($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Entry_info whereZip($value)
+ * @method static \Illuminate\Database\Query\Builder|Entry_info withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Entry_info withoutTrashed()
+ */
+	class Entry_info extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $is_admin
+ * @property int $is_staff
+ * @property string|null $is_commi
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property string|null $face_picture
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Entry_info|null $entry_info
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Sanctum\PersonalAccessToken[] $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereFacePicture($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereIsAdmin($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereIsCommi($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereIsStaff($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent implements \Illuminate\Contracts\Auth\MustVerifyEmail {}
+}
+
