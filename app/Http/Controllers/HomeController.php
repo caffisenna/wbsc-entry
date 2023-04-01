c48f82f25b58c5767f6e08bcc730f320cbaf67d5
caffi <caffi.senna@gmail.com>
2023-04-01 14:52:39 +0900

    feature#27
    close #27

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Entry_info;
+use Auth;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +25,16 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        if (Auth::user()->is_admin) {
+            // 各コースの申込人数を集計してhome画面に渡す
+            $count = Entry_info::select('sc_number')->selectRaw('count(user_id) as count_sc_number')->groupBy('sc_number')->get();
+            $count = $count->sortBy('sc_number'); // 期数毎にソート
+            $div_count = Entry_info::select('division_number')->selectRaw('count(user_id) as count_division_number')->groupBy('division_number')->get();
+            $div_count = $div_count->sortBy('division_number'); // 回数毎にソート
+
+            return view('home')->with('count', $count)->with('div_count', $div_count);
+        } else {
+            return view('home');
+        }
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
+    <div class="container">
+        <div class="row">
+            @isset($count)
+                <h2>スカウトコース申込状況</h2>
+                <table class="uk-table uk-table-divider uk-table-striped">
+                    <tr>
+                        <th>期数</th>
+                        <th>人数</th>
+                    </tr>
+                    @foreach ($count as $val)
+                        <tr>
+                            <td>{{ $val->sc_number }}</td>
+                            <td>{{ $val->count_sc_number }}</td>
+                        </tr>
+                    @endforeach
+                </table>
 
+                <h2>課程別研修申込状況</h2>
+                <table class="uk-table uk-table-divider uk-table-striped">
+                    <tr>
+                        <th>回数</th>
+                        <th>人数</th>
+                    </tr>
+                    @foreach ($div_count as $val)
+                        <tr>
+                            <td>{{ $val->division_number }}</td>
+                            <td>{{ $val->count_division_number }}</td>
+                        </tr>
+                    @endforeach
+                </table>
+            @endisset
+        </div>
     </div>
-</div>
 @endsection
