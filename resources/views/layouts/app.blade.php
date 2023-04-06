a5f68eca1b37c5e7e3c25bd778cf90b68611c089
caffi <caffi.senna@gmail.com>
2023-04-06 13:23:16 +0900

    feature#39
    close #39

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,30 +51,37 @@
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item dropdown user-menu">
                     <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-                        <img src="{{ url('/images/woggle.png') }}"
-                            class="user-image img-circle elevation-2" alt="User Image">
-                        <span class="d-none d-md-inline">{{ Auth::user()->name }}</span>
+                        <img src="{{ url('/images/woggle.png') }}" class="user-image img-circle elevation-2"
+                            alt="User Image">
+                        @auth
+                            <span class="d-none d-md-inline">{{ Auth::user()->name }}</span>
+                        @endauth
                     </a>
                     <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
                         <!-- User image -->
                         <li class="user-header bg-primary">
-                            <img src="{{ url('/images/woggle.png') }}"
-                                class="img-circle elevation-2" alt="User Image">
+                            <img src="{{ url('/images/woggle.png') }}" class="img-circle elevation-2" alt="User Image">
                             <p>
-                                {{ Auth::user()->name }}
-                                <small>Member since {{ Auth::user()->created_at->format('M. Y') }}</small>
+                                @auth
+                                    {{ Auth::user()->name }}
+                                    <small>Member since {{ Auth::user()->created_at->format('M. Y') }}</small>
+                                @endauth
                             </p>
                         </li>
                         <!-- Menu Footer-->
                         <li class="user-footer">
-                            <a href="#" class="btn btn-default btn-flat">Profile</a>
-                            <a href="#" class="btn btn-default btn-flat float-right"
-                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                Sign out
-                            </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
+                            @auth
+
+
+                                <a href="#" class="btn btn-default btn-flat">Profile</a>
+                                <a href="#" class="btn btn-default btn-flat float-right"
+                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    Sign out
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            @endauth
                         </li>
                     </ul>
                 </li>
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,57 +1,58 @@
-{{-- 一般ユーザー --}}
-@unless(Auth::user()->is_admin || Auth::user()->is_staff || Auth::user()->is_commi)
-    <p class="uk-text-warning">参加申込</p>
-    <li class="nav-item">
-        <a href="{{ route('entryInfos.index') }}" class="nav-link {{ Request::is('entryInfos*') ? 'active' : '' }}">
-            <p>申込情報</p>
-        </a>
-    </li>
+{{-- 認証されてなければサイドメニューを表示しない --}}
+@auth
+    {{-- 一般ユーザー --}}
+    @unless (Auth::user()->is_admin || Auth::user()->is_staff || Auth::user()->is_commi)
+        <p class="uk-text-warning">参加申込</p>
+        <li class="nav-item">
+            <a href="{{ route('entryInfos.index') }}" class="nav-link {{ Request::is('entryInfos*') ? 'active' : '' }}">
+                <p>申込情報</p>
+            </a>
+        </li>
 
-    <p class="uk-text-warning">写真アップロード</p>
-    <li class="nav-item">
-        <a href="{{ route('face_upload.index') }}" class="nav-link {{ Request::is('face_upload*') ? 'active' : '' }}">
-            <p>顔写真</p>
-        </a>
-    </li>
-@endunless
+        <p class="uk-text-warning">写真アップロード</p>
+        <li class="nav-item">
+            <a href="{{ route('face_upload.index') }}" class="nav-link {{ Request::is('face_upload*') ? 'active' : '' }}">
+                <p>顔写真</p>
+            </a>
+        </li>
+    @endunless
 
-{{-- 管理者 --}}
-@if (Auth::user()->is_admin)
-    <h3 class="uk-text-warning">管理者メニュー</h3>
-    <li class="nav-item">
-        <a href="{{ url('/home') }}"
-            class="nav-link {{ Request::is('admin_entryInfos*') ? 'active' : '' }}">
-            <p>HOME</p>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a href="{{ route('admin_entryInfos.index') }}"
-            class="nav-link {{ Request::is('admin_entryInfos*') ? 'active' : '' }}">
-            <p>申込一覧</p>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a href="{{ route('admin_export') }}"
-            class="nav-link {{ Request::is('admin_export*') ? 'active' : '' }}">
-            <p>エクスポート(xlsx)</p>
-        </a>
-    </li>
-    <h3 class="uk-text-warning">事務局メニュー</h3>
-    <li class="nav-item">
-        <a href="{{ route('fee_check') }}"
-            class="nav-link {{ Request::is('fee_check*') ? 'active' : '' }}">
-            <p>参加費確認</p>
-        </a>
-    </li>
-@endif
+    {{-- 管理者 --}}
+    @if (Auth::user()->is_admin)
+        <h3 class="uk-text-warning">管理者メニュー</h3>
+        <li class="nav-item">
+            <a href="{{ url('/home') }}" class="nav-link {{ Request::is('admin_entryInfos*') ? 'active' : '' }}">
+                <p>HOME</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('admin_entryInfos.index') }}"
+                class="nav-link {{ Request::is('admin_entryInfos*') ? 'active' : '' }}">
+                <p>申込一覧</p>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('admin_export') }}" class="nav-link {{ Request::is('admin_export*') ? 'active' : '' }}">
+                <p>エクスポート(xlsx)</p>
+            </a>
+        </li>
+        <h3 class="uk-text-warning">事務局メニュー</h3>
+        <li class="nav-item">
+            <a href="{{ route('fee_check') }}" class="nav-link {{ Request::is('fee_check*') ? 'active' : '' }}">
+                <p>参加費確認</p>
+            </a>
+        </li>
+    @endif
 
-{{-- 地区コミ --}}
-@if (Auth::user()->is_commi)
-    <h3 class="uk-text-warning">地区コミ</h3>
-    <li class="nav-item">
-        <a href="{{ route('commi_entryInfos.index') }}"
-            class="nav-link {{ Request::is('commi_entryInfos*') ? 'active' : '' }}">
-            <p>申込一覧</p>
-        </a>
-    </li>
-@endif
+    {{-- 地区コミ --}}
+    @if (Auth::user()->is_commi)
+        <h3 class="uk-text-warning">地区コミ</h3>
+        <li class="nav-item">
+            <a href="{{ route('commi_entryInfos.index') }}"
+                class="nav-link {{ Request::is('commi_entryInfos*') ? 'active' : '' }}">
+                <p>申込一覧</p>
+            </a>
+        </li>
+    @endif
+
+@endauth
