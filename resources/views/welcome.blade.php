2cfcbeb5f8cab01dcca266b4fef06ed69ac64de2
caffi <caffi.senna@gmail.com>
2023-05-30 22:10:17 +0900

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -58,7 +58,7 @@
 
                     @if (Route::has('register'))
                         <a href="{{ route('register') }}"
-                            class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">ユーザー登録111</a>
+                            class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">ユーザー登録222</a>
                     @endif
                 @endauth
             </div>
