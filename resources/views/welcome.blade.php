1ced4e94a06867a5262658ea9b6123b376a5d678
caffi <caffi.senna@gmail.com>
2023-06-02 08:45:55 +0900

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>スカウトコース申込システム | HOME</title>
+    <title>スカウトコース申込システム | HOMEててて</title>
 
     <!-- Styles welcome.cssに追い出した-->
     <link rel="stylesheet" href="{{ url('/css/welcome.css') }}" />
