b4f83dd5bbb9b30680f1ae2730a7b86b20b18808
caffi <caffi.senna@gmail.com>
2023-06-07 23:24:40 +0900

    fix fonts

--- app/Http/Controllers/Entry_infoController.php
+++ app/Http/Controllers/Entry_infoController.php
@@ -222,8 +222,8 @@ class Entry_infoController extends AppBaseController
 
         $pdf = \PDF::loadView('entry_infos.pdf', compact('entryInfo'));
         $pdf->setPaper('A4');
-        // return $pdf->download();
-        return $pdf->stream();
+        return $pdf->download();
+        // return $pdf->stream();
     }
 
     public function delete_file(Request $request)
