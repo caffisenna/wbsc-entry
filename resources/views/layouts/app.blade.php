6246cf6cc050d743ea6384cf3fedbe9f88811da3
caffi <caffi.senna@gmail.com>
2023-06-11 22:27:19 +0900

    feature#88
    close #88

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -101,7 +101,7 @@
         <!-- Main Footer -->
         <footer class="main-footer">
             <div class="float-right d-none d-sm-block">
-                Copyright &copy; ボーイスカウト東京連盟 指導者養成委員会 / ICT小委員会
+                Copyright &copy; ボーイスカウト東京連盟 AIS委員会 / ICT小委員会
             </div>
         </footer>
     </div>
