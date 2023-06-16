bdcda67819f5cbe93e6769bbfb3e9f93ae4948a9
caffi <caffi.senna@gmail.com>
2023-06-16 21:29:56 +0900

    feature/419 expiredの処理#94
    close #94

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,5 +38,14 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+    public function render($request, Throwable $exception)
+    {
+        // Tokenエラーの時、ログイン画面にリダイレクトする。
+        if ($exception instanceof TokenMismatchException) {
+            return redirect(route('login'));
+        }
+
+        return parent::render($request, $exception);
     }
 }
