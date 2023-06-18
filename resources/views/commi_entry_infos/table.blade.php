96f546df5cfac28ce32d4cfa4c288859c800c67d
caffi <caffi.senna@gmail.com>
2023-06-18 22:23:49 +0900

    feature/コミ一覧画面の表示項目修正

--- resources/views/commi_entry_infos/table.blade.php
+++ resources/views/commi_entry_infos/table.blade.php
@@ -5,11 +5,9 @@
                 <th>氏名</th>
                 <th>参加</th>
                 <th>トレーナー認定</th>
-                <th>課題認定者</th>
                 <th>団委員長</th>
                 <th>地区コミ</th>
                 <th>副信書</th>
-                <th>AIS委員会</th>
                 <th>申込書</th>
             </tr>
         </thead>
@@ -23,14 +21,15 @@
                             {{ $entryInfo->entry_info->dan }}
                         </td>
                         <td>{{ $entryInfo->entry_info->sc_number }}期<br>
-                            {{ $entryInfo->entry_info->division_number }}回</td>
+                            {{ $entryInfo->entry_info->division_number }}回
+                        </td>
                         @if (empty($entryInfo->entry_info->trainer_sc_checked_at) ||
                                 empty($entryInfo->entry_info->trainer_sc_name) ||
                                 empty($entryInfo->entry_info->trainer_division_checked_at) ||
                                 empty($entryInfo->entry_info->trainer_division_name))
                             <td>
                                 <a href="{{ url('/commi/trainer_request?id=') }}{{ $entryInfo->entry_info->uuid }}"
-                                    uk-toggle class="uk-link uk-button uk-button-primary uk-button-small">認定依頼</a>
+                                    class="uk-link uk-button uk-button-primary uk-button-small">認定依頼</a>
                             </td>
                         @else
                             <td>
@@ -38,15 +37,11 @@
                             </td>
                         @endif
                         <td>
-                            SC:{{ $entryInfo->entry_info->trainer_sc_name }}<br>
-                            課程別:{{ $entryInfo->entry_info->trainer_division_name }}
-                        </td>
-                        <td>
                             @if (isset($entryInfo->entry_info->gm_checked_at))
                                 {{ $entryInfo->entry_info->gm_checked_at->format('Y-m-d') }}
                             @else
-                            <a href="{{ url('/commi/gm_request?id=') }}{{ $entryInfo->entry_info->uuid }}"
-                                uk-toggle class="uk-link uk-button uk-button-primary uk-button-small">承認依頼</a>
+                                <a href="{{ url('/commi/gm_request?id=') }}{{ $entryInfo->entry_info->uuid }}"
+                                    class="uk-link uk-button uk-button-primary uk-button-small">承認依頼</a>
                             @endif
                         </td>
                         <td>
@@ -55,17 +50,11 @@
                             @else
                                 <a href="{{ url('/commi/commi_check/?id=') }}{{ $entryInfo->entry_info->id }}"
                                     class=" uk-button uk-button-primary uk-button-small"
-                                    onclick="return confirm('{{ $entryInfo->name }}さんを承認しますか?')">承認する</a>
+                                    onclick="return confirm('{{ $entryInfo->name }}さんを推薦しますか?')">推薦する</a>
                             @endif
                         </td>
-                        <td><a href="{{ url("/commi/commi_comment?id=$entryInfo->id") }}" class="uk-button uk-button-small uk-button-primary">副信書</a></td>
-                        <td>
-                            @if (isset($entryInfo->entry_info->ais_checked_at))
-                                {{ $entryInfo->entry_info->ais_checked_at->format('Y-m-d') }}
-                            @else
-                                未承認
-                            @endif
-                        </td>
+                        <td><a href="{{ url("/commi/commi_comment?id=$entryInfo->id") }}"
+                                class="uk-button uk-button-small uk-button-primary">副信書</a></td>
                         <td>
                             <div class='btn-group'>
                                 <a href="{{ url('/commi/pdf/?id=') }}{{ $entryInfo->entry_info->user_id }}"
