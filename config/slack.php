34627e00872e5d71cbb2aab2331837b84e5f6135
caffi <caffi.senna@gmail.com>
2023-07-08 18:50:27 +0900

    hotfix/hide slack_url into .env

--- config/slack.php
+++ config/slack.php
@@ -9,6 +9,5 @@ return [
 
     'channel' => 'project-wb申込通知',
     'name' => '通知Bot',
-    'url' => 'https://hooks.slack.com/services/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',
-
+    'url' => env('SLACK_URL'),
 ];
