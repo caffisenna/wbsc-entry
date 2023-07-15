153f12596d27701a678ee3fdc98c0361fd6ff657
caffi <caffi.senna@gmail.com>
2023-07-15 13:47:42 +0900

    fix/typo
    認定 => 承認

--- resources/views/mail/gm_request.blade.php
+++ resources/views/mail/gm_request.blade.php
@@ -2,7 +2,7 @@
 <p>ボーイスカウト東京連盟 指導者訓練 参加申込システム {{ config('app.name') }}から自動送信しています。</p>
 <p>地区コミッショナーからの依頼により、{{ $name }}様の参加承認URLをお送り致します。</p>
 
-<h3>認定方法</h3>
+<h3>承認方法</h3>
 <a href="{{ url('/confirm/gm?uuid=') }}{{ $uuid }}">{{ url('/confirm/gm?uuid=') }}{{ $uuid }}</a>にアクセスしてください。<br>
 
 <p>恐れ入りますがもしこの登録にお心当たりが無い場合は、<a href="mailto:wb-system@scout.tokyo">wb-system@scout.tokyo</a>までご連絡ください。</p>
