d6f60ad6d1a1394bf61101d93bd01b82807996ed
caffi <caffi.senna@gmail.com>
2023-07-22 22:33:44 +0900

    hotfix/updateユーザーガイドURL

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -125,7 +125,7 @@
                         <div class="ml-12">
                             <div class="mt-2 text-gray-600 dark:text-gray-400">
                                 <ul class="uk-list">
-                                    <li><a href="https://drive.google.com/file/d/1EckqQNrQStqHOTdVbaDUSvi1YXIBWdX_/view?usp=sharing"
+                                    <li><a href="https://drive.google.com/file/d/1lyiBfHYZPWbSe_TQZFEN9P3vQuZo76dG/view?usp=sharing"
                                             target="_blank">参加者マニュアル</a></li>
                                     <li><a href="{{ url('/howto_gm') }}">団委員長 編</a></li>
                                     <li><a href="{{ url('/howto_trainer') }}">トレーナー 編</a></li>
