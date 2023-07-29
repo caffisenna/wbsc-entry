45c06e2d73592a2892e1361768ea64c80b8ffa9c
caffi <caffi.senna@gmail.com>
2023-07-29 10:50:06 +0900

    fix/display width

--- resources/views/confirm/gm.blade.php
+++ resources/views/confirm/gm.blade.php
@@ -4,7 +4,7 @@
     <section class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
-                <div class="col-sm-6">
+                <div class="col-sm-8">
                     <h1>WB研修所申込 参加承認</h1>
                 </div>
                 <div class="col-sm-6">
