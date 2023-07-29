da4114423c2fe09509695cfc788cdf650a0550d7
caffi <caffi.senna@gmail.com>
2023-07-29 22:15:32 +0900

    Update index.blade.php

--- resources/views/commi_entry_infos/index.blade.php
+++ resources/views/commi_entry_infos/index.blade.php
@@ -4,8 +4,8 @@
     <section class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>WB研修所申込書 {{ Auth::user()->name }}</h1>
+                <div class="col-sm-8">
+                    <h1>指導者訓練申込状況 {{ Auth::user()->name }}</h1>
                 </div>
                 <div class="col-sm-6">
                 </div>
