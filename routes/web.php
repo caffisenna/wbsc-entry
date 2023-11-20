56f6f2d02f3328bf23010c24021cdfddea1e8f35
caffi <caffi.senna@gmail.com>
2023-11-20 23:26:36 +0900

    using namespaces to reduce code redundancy

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,13 @@ use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
 use App\Http\Controllers\UploadController;
 use App\Http\Controllers\FaceUploadController;
+use App\Http\Controllers\AdminEntry_infoController;
+use App\Http\Controllers\add_userController;
+use App\Http\Controllers\course_listController;
+use App\Http\Controllers\division_listController;
+use App\Http\Controllers\ConfirmController;
+use App\Http\Controllers\Entry_infoController;
+use App\Http\Controllers\CommiEntry_infoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,12 +33,12 @@ Route::get('/about', function () {
 });
 
 // トレーナー認定
-Route::get('/confirm/trainer', [App\Http\Controllers\ConfirmController::class, 'trainer_confirm'])->name('trainer_confirm');
-Route::post('/confirm/trainer', [App\Http\Controllers\ConfirmController::class, 'trainer_confirm_post'])->name('trainer_confirm_post');
+Route::get('/confirm/trainer', [ConfirmController::class, 'trainer_confirm'])->name('trainer_confirm');
+Route::post('/confirm/trainer', [ConfirmController::class, 'trainer_confirm_post'])->name('trainer_confirm_post');
 
 // 団委員長承認
-Route::get('/confirm/gm', [App\Http\Controllers\ConfirmController::class, 'gm_confirm'])->name('gm_confirm');
-Route::post('/confirm/gm', [App\Http\Controllers\ConfirmController::class, 'gm_confirm_post'])->name('gm_confirm_post');
+Route::get('/confirm/gm', [ConfirmController::class, 'gm_confirm'])->name('gm_confirm');
+Route::post('/confirm/gm', [ConfirmController::class, 'gm_confirm_post'])->name('gm_confirm_post');
 
 // 使い方ガイド
 Route::get('/howto_gm', function () {
@@ -70,44 +77,44 @@ Route::middleware('verified')->group(function () {
 
     // 一般ユーザ用
     Route::prefix('user')->group(function () {
-        Route::resource('entryInfos', App\Http\Controllers\Entry_infoController::class);
-        Route::get('/pdf', [App\Http\Controllers\Entry_infoController::class, 'pdf'])->name('pdf');
+        Route::resource('entryInfos', Entry_infoController::class);
+        Route::get('/pdf', [Entry_infoController::class, 'pdf'])->name('pdf');
         Route::resource('upload', UploadController::class);
         Route::resource('face_upload', FaceUploadController::class);
-        Route::get('/delete_file', [App\Http\Controllers\Entry_infoController::class, 'delete_file'])->name('delete_file'); // ファイルの削除
+        Route::get('/delete_file', [Entry_infoController::class, 'delete_file'])->name('delete_file'); // ファイルの削除
     });
     // 管理ユーザ用
     Route::prefix('admin')->middleware('can:admin')->group(function () {
-        Route::resource('admin_entryInfos', App\Http\Controllers\AdminEntry_infoController::class);
-        Route::get('/pdf', [App\Http\Controllers\AdminEntry_infoController::class, 'pdf'])->name('admin_pdf');
-        Route::get('/multi_pdf', [App\Http\Controllers\AdminEntry_infoController::class, 'multi_pdf'])->name('multi_pdf');
-        Route::get('/ais_check', [App\Http\Controllers\AdminEntry_infoController::class, 'ais_check'])->name('ais_check');
-        Route::get('/admin_export', [App\Http\Controllers\AdminEntry_infoController::class, 'admin_export'])->name('admin_export');
-        Route::get('/fee_check', [App\Http\Controllers\AdminEntry_infoController::class, 'fee_check'])->name('fee_check');
-        Route::get('/revert', [App\Http\Controllers\AdminEntry_infoController::class, 'revert'])->name('revert'); // 取り消し
-        Route::get('/accept', [App\Http\Controllers\AdminEntry_infoController::class, 'accept'])->name('accept'); // 参加承認
-        Route::get('/certificate', [App\Http\Controllers\AdminEntry_infoController::class, 'certificate'])->name('certificate'); // 修了認定
-        Route::resource('courseLists', App\Http\Controllers\course_listController::class); // スカウトコース設定
-        Route::resource('divisionLists', App\Http\Controllers\division_listController::class); // 課程別研修設定
-        Route::match(['get','post'],'/add_users/password_reset', [App\Http\Controllers\add_userController::class, 'pass_reset'])->name('pass_reset'); // passwordリセット
-        Route::resource('add_users', App\Http\Controllers\add_userController::class); // ユーザー追加
-        Route::get('/email_not_verified', [App\Http\Controllers\AdminEntry_infoController::class, 'email_not_verified'])->name('email_not_verified'); // メール未認証
-        Route::get('/health_memo', [App\Http\Controllers\AdminEntry_infoController::class, 'health_memo'])->name('health_memo'); // 健康上の特記事項
+        Route::resource('admin_entryInfos', AdminEntry_infoController::class);
+        Route::get('/pdf', [AdminEntry_infoController::class, 'pdf'])->name('admin_pdf');
+        Route::get('/multi_pdf', [AdminEntry_infoController::class, 'multi_pdf'])->name('multi_pdf');
+        Route::get('/ais_check', [AdminEntry_infoController::class, 'ais_check'])->name('ais_check');
+        Route::get('/admin_export', [AdminEntry_infoController::class, 'admin_export'])->name('admin_export');
+        Route::get('/fee_check', [AdminEntry_infoController::class, 'fee_check'])->name('fee_check');
+        Route::get('/revert', [AdminEntry_infoController::class, 'revert'])->name('revert'); // 取り消し
+        Route::get('/accept', [AdminEntry_infoController::class, 'accept'])->name('accept'); // 参加承認
+        Route::get('/certificate', [AdminEntry_infoController::class, 'certificate'])->name('certificate'); // 修了認定
+        Route::resource('courseLists', course_listController::class); // スカウトコース設定
+        Route::resource('divisionLists', division_listController::class); // 課程別研修設定
+        Route::match(['get','post'],'/add_users/password_reset', [add_userController::class, 'pass_reset'])->name('pass_reset'); // passwordリセット
+        Route::resource('add_users', add_userController::class); // ユーザー追加
+        Route::get('/email_not_verified', [AdminEntry_infoController::class, 'email_not_verified'])->name('email_not_verified'); // メール未認証
+        Route::get('/health_memo', [AdminEntry_infoController::class, 'health_memo'])->name('health_memo'); // 健康上の特記事項
     });
 
     // 地区コミ用
     Route::prefix('commi')->middleware('can:commi,admin')->group(function () {
-        Route::resource('commi_entryInfos', App\Http\Controllers\CommiEntry_infoController::class);
-        Route::get('/pdf', [App\Http\Controllers\CommiEntry_infoController::class, 'pdf'])->name('commi_pdf');
-        Route::get('/commi_check', [App\Http\Controllers\CommiEntry_infoController::class, 'commi_check'])->name('commi_check');
-        Route::get('/trainer_request', [App\Http\Controllers\CommiEntry_infoController::class, 'trainer_request'])->name('trainer_request');
-        Route::post('/trainer_request_send', [App\Http\Controllers\CommiEntry_infoController::class, 'trainer_request_send'])->name('trainer_request_send');
-        Route::get('/gm_request', [App\Http\Controllers\CommiEntry_infoController::class, 'gm_request'])->name('gm_request');
-        Route::post('/gm_request_send', [App\Http\Controllers\CommiEntry_infoController::class, 'gm_request_send'])->name('gm_request_send');
-        Route::get('/commi_comment', [App\Http\Controllers\CommiEntry_infoController::class, 'commi_comment'])->name('commi_comment'); // 副申請書
-        Route::post('/commi_comment', [App\Http\Controllers\CommiEntry_infoController::class, 'commi_comment_post'])->name('commi_comment_post'); // 副申請書
+        Route::resource('commi_entryInfos', CommiEntry_infoController::class);
+        Route::get('/pdf', [CommiEntry_infoController::class, 'pdf'])->name('commi_pdf');
+        Route::get('/commi_check', [CommiEntry_infoController::class, 'commi_check'])->name('commi_check');
+        Route::get('/trainer_request', [CommiEntry_infoController::class, 'trainer_request'])->name('trainer_request');
+        Route::post('/trainer_request_send', [CommiEntry_infoController::class, 'trainer_request_send'])->name('trainer_request_send');
+        Route::get('/gm_request', [CommiEntry_infoController::class, 'gm_request'])->name('gm_request');
+        Route::post('/gm_request_send', [CommiEntry_infoController::class, 'gm_request_send'])->name('gm_request_send');
+        Route::get('/commi_comment', [CommiEntry_infoController::class, 'commi_comment'])->name('commi_comment'); // 副申請書
+        Route::post('/commi_comment', [CommiEntry_infoController::class, 'commi_comment_post'])->name('commi_comment_post'); // 副申請書
         // 以下2行 地区内優先順位ソート
-        Route::get('/priority', [App\Http\Controllers\CommiEntry_infoController::class, 'priority'])->name('priority');
-        Route::post('/priority_sortable', [App\Http\Controllers\CommiEntry_infoController::class, 'priority_sortable'])->name('priority_sortable');
+        Route::get('/priority', [CommiEntry_infoController::class, 'priority'])->name('priority');
+        Route::post('/priority_sortable', [CommiEntry_infoController::class, 'priority_sortable'])->name('priority_sortable');
     });
 });
