d2de922e4af97e995bb1ce25d51aae7453bce7a7
caffi <caffi.senna@gmail.com>
2023-12-28 22:51:01 +0900

    feature/「名簿作成に同意」#115
    close #115

--- resources/views/entry_infos/create.blade.php
+++ resources/views/entry_infos/create.blade.php
@@ -26,7 +26,14 @@
                 </div>
 
             </div>
-
+            <div class="uk-card">
+                <div class="uk-card-header">
+                    <h3 class="uk-card-title">参加者名簿の作成</h3>
+                </div>
+                <div class="uk-card-body">
+                    {!! Form::checkbox('meibo', 'meibo', false, ['class' => 'uk-checkbox', 'required']) !!}参加コースで名簿を作成し、参加者及びスタッフに配布することに同意します。(掲載項目は氏名、所属、役務等)
+                </div>
+            </div>
             <div class="card-footer">
                 <p class="uk-text-danger">私、{{ Auth::user()->name }}は参加者説明会が開催される場合は参加義務があることを承知の上で研修に申し込みます。</p>
                 {!! Form::submit('登録する', ['class' => 'btn btn-primary', 'onclick' => 'window.onbeforeunload=null']) !!}
