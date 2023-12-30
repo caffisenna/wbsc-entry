9cbb261505446b2558389071fc34ae704e69a4cb
caffi <caffi.senna@gmail.com>
2023-12-30 14:06:13 +0900

    update telescope user

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -64,7 +64,8 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     {
         Gate::define('viewTelescope', function ($user) {
             return in_array($user->email, [
-                //
+                'caffi.senna@gmail.com',
+                'taichi.tadano@gmail.com'
             ]);
         });
     }
