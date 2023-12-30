ffadb96f6601fca4362b0bed23f1bdc3c3d28fa0
caffi <caffi.senna@gmail.com>
2023-12-30 21:01:53 +0900

    feature/プライバシーポリシーを掲載したい #75
    close #75

--- resources/views/entry_infos/create.blade.php
+++ resources/views/entry_infos/create.blade.php
@@ -21,6 +21,9 @@
 
             <div class="card-body">
                 <p class="uk-text-danger">私、{{ Auth::user()->name }}は参加者説明会が開催される場合は参加義務があることを承知の上で研修に申し込みます。</p>
+                <p class=""><a href="#modal-personal_info" uk-toggle><span uk-icon="icon: info"></span>個人情報の取り扱いについて</a>
+                </p>
+
                 <div class="row">
                     @include('entry_infos.fields')
                 </div>
@@ -28,10 +31,10 @@
             </div>
             <div class="uk-card">
                 <div class="uk-card-header">
-                    <h3 class="uk-card-title">参加者名簿の作成</h3>
+                    <h3 class="uk-card-title">履修者名簿の作成</h3>
                 </div>
                 <div class="uk-card-body">
-                    {!! Form::checkbox('meibo', 'meibo', false, ['class' => 'uk-checkbox', 'required']) !!}参加コースで名簿を作成し、参加者及びスタッフに配布することに同意します。(掲載項目は氏名、所属、役務等)
+                    {!! Form::checkbox('meibo', 'meibo', false, ['class' => 'uk-checkbox', 'required']) !!}参加コースで履修者名簿を作成し、参加者及びスタッフに配布することに同意します。(掲載項目は氏名、所属、役務等)
                 </div>
             </div>
             <div class="card-footer">
@@ -42,6 +45,21 @@
 
             {!! Form::close() !!}
 
+        </div>
+    </div>
+    <div id="modal-personal_info" uk-modal>
+        <div class="uk-modal-dialog uk-modal-body">
+            <h3 class="uk-modal-title">個人情報の取り扱いについて</h3>
+            当システムで収集した情報はAIS委員会、当該訓練、東京連盟事務局において指導者訓練に関する範囲で使用します。
+            <ul>
+                <li>東京連盟事務局より日本連盟に履修者名簿を提出します</li>
+                <li>東京連盟事務局で履修者名簿を保管します</li>
+                <li>東京連盟年次総会に履修者名を掲載します</li>
+                <li>コース修了後一定期間の後、入力データ及びアップロードされたファイルを事前の告知なく削除します</li>
+            </ul>
+            <p class="uk-text-right">
+                <button class="uk-button uk-button-primary uk-modal-close" type="button">閉じる</button>
+            </p>
         </div>
     </div>
 @endsection
