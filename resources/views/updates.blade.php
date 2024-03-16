90f85680a7e809629ddd539876a218ce958144cb
caffi <caffi.senna@gmail.com>
2024-03-16 22:40:37 +0900

    add 2005 on birth year

--- resources/views/updates.blade.php
+++ resources/views/updates.blade.php
@@ -4,6 +4,8 @@
     <div class="container">
         <h2>更新情報</h2>
         <ul class="uk-list uk-list-bullet">
+            <li><span class="uk-text-small uk-text-bold">2024/03/16</span><br>
+                生年月日に2005年の選択肢を追加しました。</li>
             <li><span class="uk-text-small uk-text-bold">2024/03/09</span><br>
                 [地区コミ] 参加決定通知の送信先に団委員長宛のCCが設定されている場合、参加者の詳細が表示できない不具合(500 Server Error)が解消しました</li>
             <li><span class="uk-text-small uk-text-bold">2024/03/06</span><br>
