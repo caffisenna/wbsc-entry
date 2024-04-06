c87c154e3f0d7f44d7280aef6439b0152b6b8e55
caffi <caffi.senna@gmail.com>
2024-04-06 10:39:27 +0900

    excel出力で不要な改行リファクタリング

--- resources/views/admin_export/index.blade.php
+++ resources/views/admin_export/index.blade.php
@@ -38,32 +38,39 @@
                 <td>{{ $age }}</td>
                 <td>{{ $val->bs_basic_course }}</td>
                 <td>{{ $val->scout_camp }}</td>
-                <td>{{ $val->wb_basic1_category }} {{ $val->wb_basic1_number }} {{ $val->wb_basic1_date }}
-                    <br>
-                    {{ $val->wb_basic2_category }} {{ $val->wb_basic2_number }} {{ $val->wb_basic2_date }}
-                    <br>
-                    {{ $val->wb_basic3_category }} {{ $val->wb_basic3_number }} {{ $val->wb_basic3_date }}
+                <td>
+                    {{ $val->wb_basic1_category }} {{ $val->wb_basic1_number }} {{ $val->wb_basic1_date }}
+                    @if ($val->wb_basic2_category)
+                        <br>{{ $val->wb_basic2_category }} {{ $val->wb_basic2_number }} {{ $val->wb_basic2_date }}
+                    @endif
+                    @if ($val->wb_basic3_category)
+                        <br>{{ $val->wb_basic3_category }} {{ $val->wb_basic3_number }} {{ $val->wb_basic3_date }}
+                    @endif
                 </td>
                 <td>
                     {{ $val->wb_adv1_category }} {{ $val->wb_adv1_number }} {{ $val->wb_adv1_date }}
-                    <br>
-                    {{ $val->wb_adv2_category }} {{ $val->wb_adv2_number }} {{ $val->wb_adv2_date }}
-                    <br>
-                    {{ $val->wb_adv3_category }} {{ $val->wb_adv3_number }} {{ $val->wb_adv3_date }}
+                    @if ($val->wb_adv2_category)
+                        <br>{{ $val->wb_adv2_category }} {{ $val->wb_adv2_number }} {{ $val->wb_adv2_date }}
+                    @endif
+                    @if ($val->wb_adv3_category)
+                        <br>{{ $val->wb_adv3_category }} {{ $val->wb_adv3_number }} {{ $val->wb_adv3_date }}
+                    @endif
                 </td>
                 <td>
                     {{ $val->service_hist1_role }} {{ $val->service_hist1_term }}
-                    <br>
-                    {{ $val->service_hist2_role }} {{ $val->service_hist2_term }}
-                    <br>
-                    {{ $val->service_hist3_role }} {{ $val->service_hist3_term }}
-                    <br>
-                    {{ $val->service_hist4_role }} {{ $val->service_hist4_term }}
-                    <br>
-                    {{ $val->service_hist5_role }} {{ $val->service_hist5_term }}
+                    @if ($val->service_hist2_role)
+                        <br>{{ $val->service_hist2_role }} {{ $val->service_hist2_term }}
+                    @endif
+                    @if ($val->service_hist3_role)
+                        <br>{{ $val->service_hist3_role }} {{ $val->service_hist3_term }}
+                    @endif
+                    @if ($val->service_hist4_role)
+                        <br>{{ $val->service_hist4_role }} {{ $val->service_hist4_term }}
+                    @endif
+                    @if ($val->service_hist5_role)
+                        <br>{{ $val->service_hist5_role }} {{ $val->service_hist5_term }}
+                    @endif
                 </td>
-                <td>{{ $val->health_illness }}</td>
-                <td>{{ $val->health_memo }}</td>
             </tr>
         @endforeach
     </tbody>
