2f4f7cc054794c00280c8459e7af2965d03954e3
caffi <caffi.senna@gmail.com>
2024-08-12 10:36:45 +0900

    団委員長承認依頼の画面に団名を強調表示する Fixes #184

--- resources/views/commi_entry_infos/gm_request.blade.php
+++ resources/views/commi_entry_infos/gm_request.blade.php
@@ -18,7 +18,7 @@
         @include('flash::message')
 
         <div class="clearfix"></div>
-        <p class="uk-text">{{ $userinfo->user->name }}さんについて、団委員長に承認依頼のメールを送付します。<br>
+        <p class="uk-text">{{ $userinfo->user->name }}さん({{ $userinfo->dan }})について、団委員長に承認依頼のメールを送付します。<br>
             下記フォームに団委員長の氏名、メールアドレスを入力してください。<br>
             メール以外で送信する場合は、ページ下部をご参照下さい。</p>
 
@@ -27,24 +27,27 @@
             <form method="POST" action="{{ route('gm_request_send') }}">
                 @csrf
                 <div class="uk-margin">
+                    <p class="uk-text-danger">送信先:{{ $userinfo->dan }}</p>
                     <label class="uk-form-label" for="form-stacked-text">氏名</label>
                     <div class="uk-form-controls">
-                        <input class="uk-input" id="form-stacked-text" type="text" name="name" required>
+                        <input class="uk-input" id="form-stacked-text" type="text" name="name" required
+                            placeholder="{{ $userinfo->dan }}の団委員長氏名">
                     </div>
                 </div>
 
                 <div class="uk-margin">
                     <label class="uk-form-label" for="form-stacked-text">email</label>
                     <div class="uk-form-controls">
-                        <input class="uk-input" id="form-stacked-text" type="text" name="email" required>
+                        <input class="uk-input" id="form-stacked-text" type="text" name="email" required
+                            placeholder="{{ $userinfo->dan }}の団委員長メールアドレス">
                     </div>
                 </div>
 
                 <input type="hidden" name="uuid" value="{{ $userinfo->uuid }}">
-                @if($userinfo->gm_sent_at)
-                <span class="uk-text-warning uk-text-small">{{ $userinfo->gm_sent_at }}に送信済み</span>
+                @if ($userinfo->gm_sent_at)
+                    <span class="uk-text-warning uk-text-small">{{ $userinfo->gm_sent_at }}に送信済み</span>
                 @endif
-                <input type="submit" value="{{ $userinfo->gm_sent_at ? "再度依頼する" : '依頼する' }}"
+                <input type="submit" value="{{ $userinfo->gm_sent_at ? '再度依頼する' : '依頼する' }}"
                     class="uk-button uk-button-primary uk-width-1-1@m">
                 <button class="uk-button uk-button-default uk-modal-close uk-width-1-1@m" type="button"
                     onclick="history.back()">キャンセル</button>
